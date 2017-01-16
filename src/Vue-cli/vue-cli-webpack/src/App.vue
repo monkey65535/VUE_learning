7dcb57d4bd5d261c69fb658c248f24bd92d07232
dilidili <466311467@qq.com>
2017-01-16 22:50:42 +0800

    vue-cli

---
+++ src/Vue-cli/vue-cli-webpack/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <hello></hello>
+    <h3>welcome to Vue!</h3>
+    <strong>{{message}}</strong>
+    <News></News>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import News from './components/News'
+export default {
+  data () {
+    return {
+      message: '欢迎来到Vue的世界'
+    }
+  },
+  components: {
+    Hello,
+    News
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/Vue-cli/vue-cli-webpack/src/components/News.vue
@@ -0,0 +1,3 @@
+<template>
+    <h3>this is a news page</h3>
+</template>
